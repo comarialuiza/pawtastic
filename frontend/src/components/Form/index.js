48fc806ef0fa5179f057ff556083ef6af6761488
Maria Luiza <co.maria.luiza@gmail.com>
2020-05-17 21:24:22 -0300

    Added navigation buttons

--- frontend/src/components/Form/index.js
+++ frontend/src/components/Form/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
-import { Container, MainTit } from './styles';
+import { Container, MainTit, Navigation, Button } from './styles';
 
 function Form() {
   return (
       <Container>
           <MainTit>Yay, we love dogs! Give us the basics about your pup.</MainTit>
+
+          <form>
+            <label>
+              Name
+              <input placeholder="Pet's name"/>
+            </label>
+
+            <label>
+              Breed
+              <input placeholder="Pet's breed"/>
+            </label>
+
+            <label>
+              Birthday
+              <input type="date" placeholder="MM/DD/YYYY"/>
+            </label>
+          </form>
+
+          <Navigation>
+            <Button fill={ false }>Back</Button>
+            <Button fill={ true }>Next</Button>
+          </Navigation>
       </Container>
   );
 }
--- frontend/src/components/Form/styles.js
+++ frontend/src/components/Form/styles.js
@@ -5,6 +5,9 @@ export const Container = styled.div`
     background-color: #F7EBEB;
     border-radius: 0 40px 40px 0;
     width: 72%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 `;
 
 export const MainTit = styled.h2`
@@ -12,4 +15,23 @@ export const MainTit = styled.h2`
     line-height: 32px;
     margin-bottom: 30px;
     color: #545870;
+`;
+
+export const Navigation = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 50px;
+`;
+
+export const Button = styled.button`
+    background-color: ${ props => props.fill ? '#545971' : 'transparent' };
+    color: ${ props => props.fill ? '#fff' : '#545971' };
+    border: 0;
+    border-radius: 50px;
+    border: 1px solid #545971;
+    padding: 15px 50px;
+    font-size: 12px;
+    outline: 0;
+    text-transform: uppercase;
 `;
